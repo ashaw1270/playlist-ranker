a58926e27e1a12265c31ece2eaf3b9a1234b1487
Adam Shaw <ashaw1270@icloud.com>
2024-11-10 20:29:15 -0800

    Small fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/dev_token', (req, res) => {
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'src', 'index.html'));
-})
+});
 
 app.use(express.static(path.join(__dirname, 'src')));
 
